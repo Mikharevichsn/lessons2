bf6f6c8b1de39c434847a64661ac26c1f7a541e4
Sergei Mikharevich <mikharevichsn@mail.ru>
2021-05-25 21:34:07 +0300

    lesson17

---
+++ lesson17/project2/common.js
@@ -0,0 +1,6 @@
+export const z = 11;
+export const x = 200;
+
+const main = 500;
+
+export default main;
--- lesson17/project2/index.js
+++ lesson17/project2/index.js
@@ -1,1 +1,7 @@
+import kukuruza, { z, x } from './common';
+
+console.log(z);
+console.log(x);
+console.log(kukuruza);
+
 console.log(123);
